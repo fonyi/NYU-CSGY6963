8c2a8c3587723a9ae04635ded5662b0511cb90e5
Shane Fonyi <shane@jayhawk.us>
2022-04-20 16:36:14 +0000

    assignment8

--- assignment8/historyParse.py
+++ assignment8/historyParse.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 import os
-import sqlite3
 import sys
-import pysqlite3
+import sqlite3
+import datetime
 
 #Read in exactly 1 argument
 if len(sys.argv) == 2:
@@ -17,4 +17,27 @@ except OSError:
     print('Error! - File Not Found!')
     exit()
 
+cur = s.cursor()
+
+print('Source File: '+history_file)
+
+cur.execute('SELECT COUNT(*) FROM "main"."downloads"')
+print('Total Downloads: '+str(cur.fetchone()[0]))
+
+cur.execute('SELECT * FROM "main"."downloads";')
+downloads = cur.fetchall()
+print(downloads)
+
+cur.execute('SELECT COUNT (DISTINCT term) FROM "main"."keyword_search_terms"')
+print('Unique Search Terms: '+ str(cur.fetchone()[0]))
+
+cur.execute('SELECT term FROM "main"."keyword_search_terms"  ORDER BY "url_id" DESC LIMIT 1;')
+print('Most Recent Search: '+cur.fetchone()[0])
+
+cur.execute('SELECT url_id FROM "main"."keyword_search_terms"  ORDER BY "url_id" DESC LIMIT 1;')
+url_id = cur.fetchone()[0]
+cur.execute('SELECT last_visit_time from "main"."urls" WHERE id = '+str(url_id)+';')
+search_timestamp = cur.fetchone()[0]
+search_time = (datetime.datetime(1601, 1, 1) + datetime.timedelta(microseconds=search_timestamp))
+print("Most Recent Search Date/Time: "+search_time.strftime('%Y-%b-%d %X'))
 s.close()
